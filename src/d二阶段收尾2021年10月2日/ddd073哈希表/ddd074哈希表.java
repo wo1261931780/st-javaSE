aff44930c1de3ac70d421d518b26329e4503e183
junw <45444154+wo1261931780@users.noreply.github.com>
2021-10-07 23:48:57 +0800

    2day6-10
    2021年10月7日23:48:55

--- src/d二阶段收尾2021年10月2日/ddd073哈希表/ddd074哈希表.java
+++
@@ -1,5 +0,0 @@
-package d二阶段收尾2021年10月2日.ddd073哈希表;
-
-public class ddd074哈希表 {
-    
-}
---
+++ src/d二阶段收尾2021年10月2日/ddd079linkedhashset/ddd080linkedhashset.java
@@ -0,0 +1,27 @@
+package d二阶段收尾2021年10月2日.ddd079linkedhashset;
+
+import java.util.LinkedHashSet;
+
+/**
+ * LinkedHashSet集合特点
+ * 1:哈希表和链表实现的Set接口，具有可预测的迭代次序
+ * 2:由链表保证元素有序，也就是说元素的存储和取出顺序是一致的
+ * 3:由哈希表保证元素唯一，也就是说没有重复的元素
+ */
+
+public class ddd080linkedhashset {
+    public static void main(String[] args) {
+        LinkedHashSet<String> x = new LinkedHashSet<>();
+        x.add("a1");
+        x.add("a2");
+        x.add("a3");
+        x.add("a4");
+        x.add("a5");
+        x.add("a5");
+        x.add("a5");
+        for (String i : x) {
+            System.out.println(i);
+        }
+    }
+
+}
