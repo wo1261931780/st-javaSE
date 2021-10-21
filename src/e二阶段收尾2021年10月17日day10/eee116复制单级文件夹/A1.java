ce73ec8fcc31c3b5049d2e0a8cfbb6afdc89e3f5
junw <45444154+wo1261931780@users.noreply.github.com>
2021-10-21 11:04:29 +0800

    finish

---
+++ src/e二阶段收尾2021年10月17日day10/eee116复制单级文件夹/A1.java
@@ -0,0 +1,10 @@
+package e二阶段收尾2021年10月17日day10.eee116复制单级文件夹;
+
+public class A1 {
+    public static void main(String[] args) {
+        int i = 7;
+        int j = 8;
+        int n = (i | j) % (i & j);
+        System.out.println(n);
+    }
+}
