9ae7f1e56d55f4c149fa53751fc247d138fa546b
junw <45444154+wo1261931780@users.noreply.github.com>
2021-10-29 21:08:34 +0800

    练习1
    2021年10月29日21:07:56
    学生管理系统，计算阶乘，方法重载

---
+++ src/e二阶段收尾2021年10月17日day10/eee118自己出题1/demo1.java
@@ -0,0 +1,20 @@
+package e二阶段收尾2021年10月17日day10.eee118自己出题1;
+
+import java.util.Random;
+
+public class demo1 {
+    public static void main(String[] args) {
+        Random x = new Random();
+        for (int i = 0; i < 3; i++) {
+            int x1 = x.nextInt(25);
+            System.out.println(x1);
+        }
+        // 2021年10月29日19:10:05
+        // ===========4，7，17
+        // 学生管理系统，重写一遍
+        // for循环计算阶乘，一共四种循环方法
+        // 写一个方法重载的案例
+
+
+    }
+}
---
+++ src/e二阶段收尾2021年10月17日day10/eee118自己出题1/demo_chongzai.java
@@ -0,0 +1,24 @@
+package e二阶段收尾2021年10月17日day10.eee118自己出题1;
+
+public class demo_chongzai {
+    public static void main(String[] args) {
+
+        show(1);
+        show(1, 2);
+        show(1, 2, 3);
+        System.out.println("结束");
+
+    }
+
+    public static void show(int x) {
+        System.out.println(x);
+    }
+
+    public static void show(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    public static void show(int x, int y, int z) {
+        System.out.println(x + y + z);
+    }
+}
