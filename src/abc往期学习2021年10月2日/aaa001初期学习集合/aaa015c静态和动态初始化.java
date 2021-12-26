aab3180dc1bf9be6f23c2aee441a2dc12eb31340
junw-gitkraken <wo1261931780@gmail.com>
2021-12-26 21:25:37 +0800

    1
    1

--- src/abc往期学习2021年10月2日/aaa001初期学习集合/aaa015c静态和动态初始化.java
+++ src/abc往期学习2021年10月2日/aaa001初期学习集合/aaa015c静态和动态初始化.java
@@ -36,5 +36,34 @@ public class aaa015c静态和动态初始化 {
         }
         System.out.println("最大值为：" + max);
         System.out.println("最小值为：" + min);
+        // 2021年12月26日15:17:41，补充学习
+        // 目标：掌握动态初始化元素默认值的规则。
+        // 1、整型数组的元素默认值都是0
+        int[] arrr = new int[10];
+        System.out.println(arrr[0]);
+        System.out.println(arrr[9]);
+
+        // 2、字符数组的元素默认值是多少呢？ 0
+        char[] chars = new char[100];
+        System.out.println((int)chars[0]);
+        System.out.println((int)chars[99]);
+
+        // 3、浮点型数组的元素默认值是0.0
+        double[] scores = new double[90];
+        System.out.println(scores[0]);
+        System.out.println(scores[89]);
+
+        // 4、布尔类型的数组
+        boolean[] booleans = new boolean[100];
+        System.out.println(booleans[0]);
+        System.out.println(booleans[99]);
+
+        // 5、引用类型的数组
+        String[] names = new String[90];
+        System.out.println(names[0]);
+        System.out.println(names[89]);
+
+        // int[] arrs = new int[3]{30, 40, 50};
+        //静态和动态初始化，二者不可混用
     }
 }
--- src/abc往期学习2021年10月2日/aaa001初期学习集合/aaa016遍历数组数字.java
+++ src/abc往期学习2021年10月2日/aaa001初期学习集合/aaa016遍历数组数字.java
@@ -1,5 +1,8 @@
 package abc往期学习2021年10月2日.aaa001初期学习集合;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class aaa016遍历数组数字 {
     public static void main(String[] args) {
         System.out.println("开始定义数组:");
@@ -19,6 +22,98 @@ public class aaa016遍历数组数字 {
             System.out.println("第" + x + "个数是：" + xx[x]);
             //System.out.println(xx[x]);
             System.out.println("重复显示--------------------");
+        }
+        // show();
+        // sum();
+        // mx();
+        // guessnum();
+        rank();
+    }
+
+    public static void show() {
+        int[] x = new int[9];
+        x[8] = 11;
+        for (int i = 0; i < 9; i++) {
+            System.out.println(x[i]);
+        }
+        System.out.println("结束");
+    }
+
+    // 直接使用数组名.fori就会自动生成一个遍历循环
+    public static void sum() {
+        int[] x = {1, 1, 1, 1, 1, 1, 1};
+        System.out.println(x.length);
+        int sim = 0;
+        for (int i = 0; i < x.length; i++) {
+            sim += x[i];
+        }
+        System.out.println(sim);
+    }
+
+    public static void mx() {
+        int[] x = {11, 1, 56, 4, 165, 1, 65, 4, 8, 498};
+        int max = 0;
+        int min = 0;
+        for (int i = 0; i < x.length; i++) {
+            max = max < x[i] ? x[i] : max;
+            min = min > x[i] ? x[i] : min;
+            // if(max<x[i]){
+            //     max=x[i];
+            // }else if(min>x[i]){
+            //     min=x[i];
+            // }
+        }
+        System.out.println(max + "***" + min);
+    }
+
+    public static void guessnum() {
+        Random xx = new Random();
+        int[] x = new int[5];
+        for (int i = 0; i < 5; i++) {
+            int i1 = xx.nextInt(20);
+            x[i] = i1;
+        }
+        Scanner x1 = new Scanner(System.in);
+        OUT:
+// 循环结束的位置
+        while (true) {
+            System.out.println("请输出你的数:");
+            int i = x1.nextInt();
+            for (int j = 0; j < x.length; j++) {
+                if (i == x[j]) {
+                    System.out.println("right num");
+                    // for (int i1 = 0; i1 < x.length; i1++) {
+                    //     System.out.println(x[i1]);
+                    // }
+                    break OUT;//直接结束到out位置
+                }
+            }
+        }
+    }
+
+    public static void rank() {//冒泡排序
+        int[] x={1,1,1,321,5,54,4,85,64};
+        for (int i = 0,mid=0; i < x.length-1; i++) {
+            // if (x[i]>x[i+1]){
+            //     mid=x[i];
+            //     x[i]=x[i+1];
+            //     x[i+1]=mid;
+            // }
+            // i == 0  比较的次数 3  j = 0 1 2
+            // i == 1  比较的次数 2  j = 0 1
+            // i == 2  比较的次数 1  j = 0
+            // 3、定义一个循环控制每轮比较的次数，占位
+            // 如果只有单个循环，只会遍历一遍，两两交换，循环嵌套则可以实现从小到大的排序
+            for (int j = 0; j < x.length-i-1; j++) {
+                if (x[j]>x[j+1]){
+                    mid=x[j];
+                    x[j]=x[j+1];
+                    x[j+1]=mid;
+                }
+            }
+        }
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i]+"--");
         }
     }
 }
