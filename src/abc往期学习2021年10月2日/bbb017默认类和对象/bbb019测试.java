7e068f654ede44616a86d21d6a2c6a888db2087b
junw <45444154+wo1261931780@users.noreply.github.com>
2022-01-06 22:26:55 +0800

    fix

--- src/abc往期学习2021年10月2日/bbb017默认类和对象/bbb019测试.java
+++ src/abc往期学习2021年10月2日/bbb017默认类和对象/bbb019测试.java
@@ -2,9 +2,10 @@ package abc往期学习2021年10月2日.bbb017默认类和对象;
 
 public class bbb019测试 {
     public static void main(String[] args) {
-        bbb018 x=new bbb018();//如果没有给出构造方法，自动出现一个默认构造方法
+        //如果没有给出构造方法，自动出现一个默认构造方法
+        bbb018 x = new bbb018();
         x.show();
-        bbb018 x1=new bbb018("123",33);
+        bbb018 x1 = new bbb018("123", 33);
         //带参构造方法，可以得到直接指定具体参数的对象。
         x1.show();
     }
