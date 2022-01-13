90dbeef62805a6f53cc1c0dabe6ca8d03bb08075
junw <45444154+wo1261931780@users.noreply.github.com>
2022-01-13 17:45:54 +0800

    day11-9

---
+++ src/c2stage_20220113/ccc008extend_standard/ccc001role.java
@@ -0,0 +1,25 @@
+package c2stage_20220113.ccc008extend_standard;
+
+public class ccc001role {
+    private String name;
+    private int age;
+    public void search_class(){
+        System.out.println("ËÑË÷¿Î³Ì");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/c2stage_20220113/ccc008extend_tedian/ccc001.java
@@ -0,0 +1,4 @@
+package c2stage_20220113.ccc008extend_tedian;
+
+public class ccc001 {
+}
