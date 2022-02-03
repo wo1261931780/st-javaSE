274396bce51350f395b87ed54b48e6c603b7c0bc
junw <45444154+wo1261931780@users.noreply.github.com>
2022-02-03 22:49:49 +0800

    day14

---
+++ src/c2stage_20220203/ccc040api_date/ccc001date.java
@@ -0,0 +1,10 @@
+package c2stage_20220203.ccc040api_date;
+
+import java.util.Date;
+
+public class ccc001date {
+    public static void main(String[] args) {
+        Date x=new Date();
+        System.out.println(" ±º‰ «£∫"+x);
+    }
+}
