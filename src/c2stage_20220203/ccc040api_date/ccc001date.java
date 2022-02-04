1966523237226d03f06e4693d3b5fad41b0bec4f
junw <45444154+wo1261931780@users.noreply.github.com>
2022-02-04 21:04:21 +0800

    day14

--- src/c2stage_20220203/ccc040api_date/ccc001date.java
+++ src/c2stage_20220203/ccc040api_date/ccc001date.java
@@ -11,9 +11,11 @@ public class ccc001date {
         System.out.println("**********************");
         // 其实每次命令执行都是有时间的，为什么这个时间没有计算进入毫秒值
         Date x2 = new Date();
-        x2.setTime(x1);
+        x2.setTime(x1);//这里set可以直接使用毫秒值，也可以使用new出来的默认date
         System.out.println(x2);//Thu Feb 03 23:03:22 CST 2022
         //实际上，是新建了一个date类型的对象
         // 而这个date类型的，内部有get/set方法
+        Date x3 = new Date(x1);//直接传入一个确定的时间作为参数，进行设置
+        System.out.println(x3);
     }
 }
---
+++ src/c2stage_20220203/ccc042date_case/ccc001case.java
@@ -0,0 +1,7 @@
+package c2stage_20220203.ccc042date_case;
+
+public class ccc001case {
+    public static void main(String[] args) {
+        
+    }
+}
