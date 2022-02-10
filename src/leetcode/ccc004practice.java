a0a8b8fc303f5103ca545626fd081d2ca163c6f5
junw <45444154+wo1261931780@users.noreply.github.com>
2022-02-10 23:38:14 +0800

    tips

--- src/leetcode/ccc004practice.java
+++ src/leetcode/ccc004practice.java
@@ -31,3 +31,24 @@ public class ccc004practice {
     }
 
 }
+//µÈ²îÊýÁÐ£º
+//
+//import java.util.Scanner;
+//
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         while (in.hasNextInt()) {
+//             int n = in.nextInt();
+//             long sum = (long)Math.pow(n,3);
+//             int a1 = (int)sum/n - (n - 1);
+//             StringBuilder sb = new StringBuilder(Integer.toString(a1));
+//             for(int i = 1; i < n; i++){
+//                 a1 = a1 + 2;
+//                 sb.append("+");
+//                 sb.append(a1);
+//             }
+//             System.out.println(sb);
+//         }
+//     }
+// }
