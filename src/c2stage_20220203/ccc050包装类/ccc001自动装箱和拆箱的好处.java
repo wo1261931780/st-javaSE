6be9a954a23781234bcf146d9eb1d30f801a87a3
junw <45444154+wo1261931780@users.noreply.github.com>
2022-02-13 13:40:11 +0800

    day14-5

--- src/c2stage_20220203/ccc050包装类/ccc001自动装箱和拆箱的好处.java
+++ src/c2stage_20220203/ccc050包装类/ccc001自动装箱和拆箱的好处.java
@@ -13,5 +13,17 @@ public class ccc001自动装箱和拆箱的好处 {
         String x1 = x.toString();//因为是对象，所以可以继承object类，有tostring方法
         //tostring方法将其变成一个字符串
         System.out.println(x1);
+        // 字符串类型变成整数类型
+        String x2 = "11";
+        int x3 = Integer.parseInt(x2);
+        System.out.println(++x3);//12
+        String x4 = "123.12";
+        Double x5 = Double.parseDouble(x4);
+        System.out.println(++x5);//124.12
+        // *****************************************
+        int x6 = Integer.valueOf(x2);//这里x2必须是整数类型，否则报错
+        Double x7 = Double.valueOf(x4);//同理，包装类只能转化为对应的类型
+        //直接使用valueof进行转换即可
+
     }
 }
