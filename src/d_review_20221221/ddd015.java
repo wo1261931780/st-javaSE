60dbcc183387fc676a6be9f627a4d99f2c0a2a50
junw <45444154+wo1261931780@users.noreply.github.com>
2022-12-25 13:52:20 +0800

    日常push

---
+++ src/d_review_20221221/ddd015.java
@@ -0,0 +1,24 @@
+package d_review_20221221;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-java.github.io
+ * Package:d_review_20221221
+ *
+ * @author liujiajun_junw
+ * @Date 2022-12-17-31  星期五
+ * @description
+ */
+public class ddd015 {
+	public static void main(String[] args) {
+		ArrayList<Integer> objects = new ArrayList<>();
+		objects.add(1);
+		objects.add(2);
+		objects.add(3);
+		objects.add(4);
+		objects.add(5);
+		System.out.println(objects);
+	}
+}
