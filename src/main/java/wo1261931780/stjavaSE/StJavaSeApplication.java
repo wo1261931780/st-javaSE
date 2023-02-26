f6bd38a9eedce9cc5f6d8be21ff82a266a7fc80d
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 20:12:54 +0800

    使用spring完成项目初始化过程

---
+++ src/main/java/wo1261931780/stjavaSE/StJavaSeApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stjavaSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StJavaSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StJavaSeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stjavaSE/StJavaSeApplicationTests.java
@@ -0,0 +1,14 @@
+package wo1261931780.stjavaSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StJavaSeApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// 	System.out.println("1");
+	// }
+
+}
