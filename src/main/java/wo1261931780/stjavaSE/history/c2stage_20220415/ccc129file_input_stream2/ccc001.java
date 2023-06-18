edd16bd2d2fece88978503bb4cfc08463c21edea
junw <45444154+wo1261931780@users.noreply.github.com>
2023-06-18 08:16:05 +0800

    Update ccc001.java

--- src/main/java/wo1261931780/stjavaSE/history/c2stage_20220415/ccc129file_input_stream2/ccc001.java
+++ src/main/java/wo1261931780/stjavaSE/history/c2stage_20220415/ccc129file_input_stream2/ccc001.java
@@ -13,15 +13,15 @@ import java.io.InputStream;
  * Time:  2022-04-22-54  星期五
  */
 public class ccc001 {
-    public static void main(String[] args) throws IOException {
-        File demo = new File("src/c2stage_20220415/ccc128file_input_stream2/ccc038demo.txt");
-        InputStream x = new FileInputStream(demo);
-        System.out.println(demo.length());//343,这里是long类型的
-        byte[] x1 = new byte[(int) demo.length()];// 所以要转化为int类型
-        int demo2 = x.read(x1);
-        // System.out.println(new String(x1, 0, demo2));
-        // 中文还是读不出来
-        byte[] demobyte = x.readAllBytes();// 输入流，直接读取所有的字节文件
-        System.out.println("--" + new String(demobyte, 0, demobyte.length) + "--");
-    }
+	public static void main(String[] args) throws IOException {
+		File demo = new File("src/c2stage_20220415/ccc128file_input_stream2/ccc038demo.txt");
+		InputStream x = new FileInputStream(demo);
+		System.out.println(demo.length());//343,这里是long类型的
+		byte[] x1 = new byte[(int) demo.length()];// 所以要转化为int类型
+		int demo2 = x.read(x1);
+		// System.out.println(new String(x1, 0, demo2));
+		// 中文还是读不出来
+		byte[] demobyte = x.readAllBytes();// 输入流，直接读取所有的字节文件
+		System.out.println("--" + new String(demobyte, 0, demobyte.length) + "--");
+	}
 }
